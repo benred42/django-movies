39a86f51622d33fb3cfa730172c9cbfd922df8be
Benjamin Phillips <benred42@gmail.com>
2015-06-16 09:15:59 -0400

    Fixed timestamp to use django timezone instead of datetime

--- movieratings/Movies/forms.py
+++ movieratings/Movies/forms.py
@@ -1,13 +1,13 @@
 from django import forms
 from Movies.models import Rating, Movie
-from datetime import datetime
+from django.utils import timezone
 
 
 class RatingForm(forms.ModelForm):
     movie = forms.ModelChoiceField(Movie.objects.order_by("title"), to_field_name="title")
     rating = forms.ChoiceField(choices=((1, 1), (2, 2), (3, 3), (4, 4), (5, 5)))
     review = forms.CharField(widget=forms.Textarea, required=False)
-    timestamp = forms.DateTimeField(widget=forms.HiddenInput(), initial=datetime.now())
+    timestamp = forms.DateTimeField(widget=forms.HiddenInput(), initial=timezone.now)
 
     class Meta:
         model = Rating
